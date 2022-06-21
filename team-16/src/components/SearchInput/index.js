23572e714079186fa94b422d3a680dbc8f3705ec
multi-vit <103585624+multi-vit@users.noreply.github.com>
2022-06-21 14:48:52 +0100

    Add Text State and update it with user input

--- team-16/src/components/SearchInput/index.js
+++ team-16/src/components/SearchInput/index.js
@@ -1,14 +1,20 @@
 // 3 inputs: ConfirmSearch button, RandomSearch button, SearchFieldInput
 //
 
-import React from 'react'; 
+import React, {useState} from 'react'; 
 
 export default function SearchInput() {
+    const [Text, setText] = useState("")
+    function updateText(event){
+        setText(event.target.value);
+        console.log(Text);
+    }
+
     return(
         <React.Fragment>
             <button>Search</button>
             <button>Random</button>
-            <input type='text' placeholder='Search by Category'></input>
+            <input type='text' placeholder='Search by Category' onChange={updateText}></input>
         </React.Fragment>
     )
 }
